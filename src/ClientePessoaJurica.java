828596bc678216b9cc1368e1d20a1cfce010fb1b
RosanaBorges <77994621+RosanaBorges@users.noreply.github.com>
2022-05-16 14:10:05 -0300

    Add files via upload

---
+++ src/ClientePessoaJurica.java
@@ -0,0 +1,21 @@
+public class ClientePessoaJurica extends Cliente{
+
+    public String nome;
+    public int cnpj;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+}
